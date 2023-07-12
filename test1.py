d814a1b21972ed1cd719fb99070c29f4a213bebe
Chen <13102049086lc@gmail.com>
2023-07-12 10:38:38 +1000

    second

--- test1.py
+++ test1.py
@@ -5,4 +5,32 @@ Created on Wed Jul 12 10:21:00 2023
 @author: 13102
 """
 
-print("Hello this is my chess file :)")
+print("Hello this is my chess file.")
+
+# ls  = list the things in the current directory
+# pwd = print working directory
+# cd folder_name = change directroy to folder_name
+# cd .. = go to parent folder
+
+
+
+# saving your work
+# navigate to the chess/chess folder before doing these steps
+# git add file_name if you have made a new file
+# git commit -m "message" = commit with that message
+# git push = push to github
+
+# to set up on home computer
+# download git bash
+# make a folder that you want to work in
+# navigate to that folder in git bash
+# copy the link to your repository in github
+# git clone the_url (right click paste)
+# you may have to do authentification things
+
+print("hello again")
+
+
+
+
+
